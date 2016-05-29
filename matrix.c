e83498764ce1fdc073a6484534f6c33abbc7c506
Kouhei Ito <kouhei.ito@itolab-ktc.com>
2016-05-29 12:40:41 +0900

    今日はREADMEを編集しました

---
+++ matrix.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+//行列を表す構造体のテンプレート
+struct matrix{
+  short low;    //行列の行数
+  double* data;  //行列の中身を格納するポインタ
+}
+
+//ベクトルを表す構造体のテンプレート
+struct vector{
+  short low;    //行列の行数
+  double* data;  //行列の中身を格納するポインタ
+}
+
+short matrixCreate(short low, short column, double* matdata,struct matrix mat){
+  
+
+}
+
+main(){
+
+}
+
