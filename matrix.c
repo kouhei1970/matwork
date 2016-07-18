58e54625e2d4e3b78a6251041ce5538e9cdf6e15
Kouhei <kouhei.ito@itolab-ktc.com>
2016-07-18 13:16:47 +0900

    修正

--- matrix.c
+++ matrix.c
@@ -3,19 +3,26 @@
 
 //行列を表す構造体のテンプレート
 struct matrix{
-  short low;    //行列の行数
+  int low;    //行列の行数
+  int column; //行列の列数
   double* data;  //行列の中身を格納するポインタ
 }
 
 //ベクトルを表す構造体のテンプレート
 struct vector{
-  short low;    //行列の行数
+  int low;    //ベクトルの次元数
   double* data;  //行列の中身を格納するポインタ
 }
 
-short matrixCreate(short low, short column, double* matdata,struct matrix mat){
-  
+int matrixCreate(int low, int column, double* matdata,struct matrix mat){
+  mat.data = matdata;
+  mat.low = low;
+  mat.column = column;
+}
 
+int vectorCreate(int low, double* vectordata,struct matrix vector){
+  vector.data = vectordata;
+  vector.low = low;
 }
 
 main(){
