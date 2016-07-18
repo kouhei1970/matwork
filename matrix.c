603f422d102210ba304156630cf786cb218f6763
Kouhei <kouhei.ito@itolab-ktc.com>
2016-07-18 13:26:29 +0900

    初テスト実行用修正

--- matrix.c
+++ matrix.c
@@ -18,14 +18,38 @@ int matrixCreate(int low, int column, double* matdata,struct matrix mat){
   mat.data = matdata;
   mat.low = low;
   mat.column = column;
+  return 0;
 }
 
 int vectorCreate(int low, double* vectordata,struct matrix vector){
   vector.data = vectordata;
   vector.low = low;
+  return 0;
 }
 
-main(){
+int matrixPrint(struct matrix mat){
+  int i,j;
+  
+  for (i=0; i<mat.low; i++) {
+    for (j=0; j<mat.column; j++) {
+      printf("%10.3f ",*(mat.data));
+    }
+    printf("\n");
+  }
+  return 0;
+}
 
+int main(){
+  double a={1.0, 2.0, 3.0, 4.0};
+  double b={5.0, 6.0, 7.0, 8.0};
+  struct matrix A;
+  struct matrix B;
+  
+  matrixCreate(2,2,a,A);
+  matrixCreate(2,2,b,B);
+  
+  matrixPrint(A);
+  
+  return 0;
 }
 
